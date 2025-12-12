e439445342ddd9ffaa382e2a1510217cc1cd3a22
0245351-ai <0245351@up.edu.mx>
2025-12-12 00:16:56 -0600

    Update main.py

--- main.py
+++ main.py
@@ -151,7 +151,7 @@ df = load_data()
 # Columnas que NO queremos como categorías
 columnas_excluir = [
     'name', 'address', 'city', 'state', 'latitude', 'longitude',
-    'stars', 'review_count', 'is_open', 'attributes', 'Restaurants', 'Food', 'Nightlife'
+    'stars', 'review_count', 'is_open', 'attributes', 'Restaurants', 'Food', 'Nightlife', 'Bars'
 ]
 
 # Detectar columnas binarias (0/1)
